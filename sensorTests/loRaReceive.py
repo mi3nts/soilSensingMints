b8b68bcc3cd298149404fedba911e55cd629308b
VA_24 <60634457+VA-24@users.noreply.github.com>
2022-07-20 10:18:42 -0500

    Update loRaReceive.py

--- sensorTests/loRaReceive.py
+++ sensorTests/loRaReceive.py
@@ -2,11 +2,6 @@ import serial
 import datetime
 import os
 import csv
-#import deepdish as dd
-#from mintsXU4 import mintsLatest as mL
-#from mintsXU4 import mintsDefinitions as mD
-#from mintsXU4 import mintsSensorReader as mSR
-from getmac import get_mac_address
 import time
 import serial
 import pynmea2
@@ -16,23 +11,32 @@ import math
 import base64
 import json
 import struct
-import yaml
 from traceback import print_tb
 import paho.mqtt.client as mqtt
+import yaml
+import mintsLiveNodes as mLN
+from backports.datetime_fromisoformat import MonkeyPatch
+from datetime import date, datetime, time
+
 
 dataFolder = "/home/teamlary/mnt/teamlary1/mintsData"
 dataFolderMQTT = dataFolder +"rawMQTT"
 
 mqttPort = 1883
 mqttBroker = "mqtt.lora.trecis.cloud"
-nodeIDs = mD.nodeIDs
 connected    = False  # Stores the connection status
 broker       = mqttBroker  
 port         = mqttPort  # Secure port
 mqttUN       = "username"
 mqttPW       = "password"
 nodeObjects  = []
-decoder = json.JSONDecoder(object_pairs_hook=collections.OrderedDict)
+decoder = json.JSONDecoder(object_pairs_hook=OrderedDict)
+nodeIDsPre = yaml.load(open("credentials/nodeIDs.yaml"),Loader=yaml.FullLoader)
+nodeIDs = nodeIDsPre['nodeIDs']
+MonkeyPatch.patch_fromisoformat()
+portIDsFile         = "credentials/portIDs.yaml"
+portDefinitions     = yaml.load(open(portIDsFile),Loader=yaml.FullLoader)
+portIDs             = portDefinitions['portIDs']
 
 ###############################################
 
@@ -58,38 +62,40 @@ def loRaWriteFinisher(nodeID,sensorID,dateTime,sensorDictionary):
     #mSR.writeCSV2(writePath,sensorDictionary,exists)
     mL.writeJSONLatestMQTT(sensorDictionary,nodeID,sensorID)
     return;
-  
-def IPS7100CNRLoRaWrite(dateTime,nodeID,sensorID,framePort,base16Data):
-    if(framePort == 17 and len(base16Data) ==112) :
-        sensorDictionary =  OrderedDict([
-                ("dateTime" , str(dateTime)), 
-        		("pc0_1"  ,struct.unpack('<L',bytes.fromhex(base16Data[0:8]))[0]),
-            	("pc0_3"  ,struct.unpack('<L',bytes.fromhex(base16Data[8:16]))[0]),
-                ("pc0_5"  ,struct.unpack('<L',bytes.fromhex(base16Data[16:24]))[0]),
-                ("pc1_0"  ,struct.unpack('<L',bytes.fromhex(base16Data[24:32]))[0]),
-            	("pc2_5"  ,struct.unpack('<L',bytes.fromhex(base16Data[32:40]))[0]),
-        		("pc5_0"  ,struct.unpack('<L',bytes.fromhex(base16Data[40:48]))[0]), 
-            	("pc10_0" ,struct.unpack('<L',bytes.fromhex(base16Data[48:56]))[0]),
-        		("pm0_1"  ,struct.unpack('<f',bytes.fromhex(base16Data[56:64]))[0]), 
-            	("pm0_3"  ,struct.unpack('<f',bytes.fromhex(base16Data[64:72]))[0]),
-                ("pm0_5"  ,struct.unpack('<f',bytes.fromhex(base16Data[72:80]))[0]),
-                ("pm1_0"  ,struct.unpack('<f',bytes.fromhex(base16Data[80:88]))[0]),
-            	("pm2_5"  ,struct.unpack('<f',bytes.fromhex(base16Data[88:96]))[0]),
-        		("pm5_0"  ,struct.unpack('<f',bytes.fromhex(base16Data[96:104]))[0]), 
-            	("pm10_0" ,struct.unpack('<f',bytes.fromhex(base16Data[104:112]))[0])
-        ])
-        
-    loRaWriteFinisher(nodeID,sensorID,dateTime,sensorDictionary)  
-    print(sensorDictionary)
-    return sensorDictionary
-  
+
 def sensorReceiveLoRa(dateTime,nodeID,sensorID,framePort,base16Data):
     sensorDictionary =  OrderedDict([
                 ("dateTime" , str(dateTime))])
     
-    if(sensorID=="IPS7100CNR"):
-        sensorDictionary = IPS7100CNRLoRaWrite(dateTime,nodeID,sensorID,framePort,base16Data) 
+    if(sensorID=="SOILMOISTURE"):
+        sensorDictionary = SOILMOISTURELoRaWrite(dateTime,nodeID,sensorID,framePort,base16Data) 
+    elif(sensorID=="NPK"):
+        sensorDictionary = NPKLoRaWrite(dateTime,nodeID,sensorID,framePort,base16Data) 
     return sensorDictionary;
+  
+def SOILMOISTURELoRaWrite(dateTime,nodeID,sensorID,framePort,base16Data):
+    if(framePort == 17 and len(base16Data) == 8) :
+        sensorDictionary =  OrderedDict([
+                ("dateTime", str(dateTime)), 
+        		("SoilMoisture", struct.unpack('<L',bytes.fromhex(base16Data[0:8]))[0]), #what does <L do
+        ])
+        
+    #loRaWriteFinisher(nodeID,sensorID,dateTime,sensorDictionary)  
+    print(sensorDictionary)
+    return sensorDictionary
+
+def NPKLoRaWrite(dateTime,nodeID,sensorID,framePort,base16Data):
+    if(framePort == 25 and len(base16Data) == 24) :
+        sensorDictionary =  OrderedDict([
+                ("dateTime", str(dateTime)), 
+        		("N", struct.unpack('<L',bytes.fromhex(base16Data[0:8]))[0]),
+                ("P", struct.unpack('<L',bytes.fromhex(base16Data[8:16]))[0]),
+                ("K", struct.unpack('<L',bytes.fromhex(base16Data[16:24]))[0])
+        ])
+        
+    #loRaWriteFinisher(nodeID,sensorID,dateTime,sensorDictionary)  
+    print(sensorDictionary)
+    return sensorDictionary
 
 def getPortIndex(portIDIn,portIDs):
     indexOut = 0
@@ -114,8 +120,8 @@ def loRaSummaryReceive(message,portIDs):
     rxInfo              =  sensorPackage['rxInfo'][0]
     txInfo              =  sensorPackage['txInfo']
     loRaModulationInfo  =  txInfo['loRaModulationInfo']
-    sensorID            = portIDs[getPortIndex(sensorPackage['fPort'],portIDs)]['sensor']
-    dateTime            = datetime.datetime.fromisoformat(sensorPackage['publishedAt'][0:26])
+    sensorID            = portIDs[getPortIndex(sensorPacakage['fPort'],portIDs)]['sensor']
+    dateTime            = datetime.fromisoformat(sensorPackage['publishedAt'][0:26]) #edited with monkeypatch
     base16Data          = base64.b64decode(sensorPackage['data'].encode()).hex()
     gatewayID           = base64.b64decode(rxInfo['gatewayID']).hex()
     framePort           = sensorPackage['fPort']
@@ -161,7 +167,7 @@ def on_message(client, userdata, msg):
         print(" - - - MINTS DATA RECEIVED - - - ")
         # print(msg.payload)
         dateTime,gatewayID,nodeID,sensorID,framePort,base16Data = \
-            mLR.loRaSummaryReceive(msg,portIDs)
+            loRaSummaryReceive(msg, portIDs)
         print("Node ID         : " + nodeID)
         nodeIndex = getNodeIndex(nodeID)
         if nodeIndex >= 0 :  
